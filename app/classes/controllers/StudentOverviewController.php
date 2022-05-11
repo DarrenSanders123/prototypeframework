2d8a2fecc660cdff59c113f22d8147bf55461d54
DarrenSanders <332030@student.mboutrecht.nl>
2022-05-11 21:41:37 +0200

    Updated StudentOverviewController.php   - Make it extent Controller.php   - Updated the SQL comment   - Updated the SQL Statement   - Updated the catch to return an error, instead of nothing

--- app/classes/controllers/StudentOverviewController.php
+++ app/classes/controllers/StudentOverviewController.php
@@ -1,31 +1,35 @@
 <?php
 
-class StudentOverviewController
+class StudentOverviewController extends \DS\Controller
 {
-    function getStudents()
+    public function studentsOverview()
+    {
+        $students = $this->getStudents(); // Load the students
+        $this->view('students.overview', array('students' => $students)); // Show the view
+    }
+
+    private function getStudents()
     {
         /**
          * select * from students
          * LEFT JOIN addresses a on students.AddressId = a.AddressId
-         * INNER JOIN students_roles sr on students.StudentId = sr.StudentId
-         * WHERE students.StudentId = :id
+         * LEFT JOIN roles r on students.RoleId = r.RoleId
          */
+
         global $pdoOne; // Declare the connection
 
         try {
-            $pdoOne
-                ->select('*')
+            // Statement to get all the students and there address and role
+            return $pdoOne
+                ->select('students.StudentId, StudentName, StudentEmail, StudentMobile, Street, HouseNr, City, State, Country, r.RoleId,  RoleName, IF(r.UpdatedAt > students.UpdatedAt, r.UpdatedAt, students.UpdatedAt) as UpdatedAt')
+                ->from('students')
                 ->left('addresses a on students.AddressId = a.AddressId')
-                ->left('students_roles sr on students.StudentId = sr.StudentId')
-                ->where('students.StudentId = :id')
-                ->toList();
+                ->left('roles r on students.RoleId = r.RoleId')
+                ->toList(PDO::FETCH_OBJ);
         } catch (Exception $exception) {
             if (APP_MODE == "DEV") {
-                dd($exception);
-            } else return;
-
+                dd($exception); // If running in dev mode display the error else just show error 500
+            } else return new \DS\ErrorHandling(500);
         }
-
-
     }
 }
