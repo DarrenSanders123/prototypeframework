a5bdaf719533b51ff1c2755397e910ee5470283d
DarrenSanders <332030@student.mboutrecht.nl>
2022-05-11 21:43:33 +0200

    Updated web.php   - Removed old error route response   - Added students route group, and an overview route

--- app/routes/web.php
+++ app/routes/web.php
@@ -7,9 +7,11 @@ $router->respond('GET', '/?', function () {
     return 'INDEX!';
 });
 
-$router->respond('/error', function () {
-    Controller::view('error');
-})->setName('error');
+$router->with('/students', function () use ($router) {
+    $router->get('/?', function () {
+        (new StudentOverviewController())->studentsOverview();
+    });
+});
 
 $router->onHttpError(function ($code, $err_msg) {
     (new ErrorHandling($code))->ShowErrorScreen();
