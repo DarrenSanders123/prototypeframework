4fb919665713fcbe05681decb86a9cc896d88686
DarrenSanders <332030@student.mboutrecht.nl>
2022-05-11 21:38:24 +0200

    Updated autoload.php:   - added spaces to variable   - added blank line for spacing

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 spl_autoload_register(function($className)
 {
-    $className=str_replace("\\","/",$className);
+    $className = str_replace("\\","/",$className);
 
     $parts = explode('/', $className);
     $className = end($parts);
@@ -10,6 +10,7 @@ spl_autoload_register(function($className)
     $functions = APP_ROOT . "\\app\\classes\\functions\\{$className}.php";
     $router = APP_ROOT . "\\app\\classes\\router\\{$className}.php";
 //    $controller = APP_ROOT . "\\app\\classes\\controllers\\".(empty($namespace)?"":$namespace."\\")."{$className}.php";
+
     if (file_exists($controller)) {
         $class = $controller;
     } elseif (file_exists($functions)) {
