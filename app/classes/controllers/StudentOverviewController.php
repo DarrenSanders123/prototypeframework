a8f8f43e8ce5e2f56791bba535275da3ec091dca
DarrenSanders <332030@student.mboutrecht.nl>
2022-05-12 20:38:27 +0200

    Updated StudentOverviewController.php   - Renamed studentsOverview function to index   - Added function ApiGetStudents that returns a formatted string with all the students, and with table rows   - Added (object) classifier to the statement   - Imported ErrorHandling   - Created new function formatRows, that gets the students and returns formatted html

--- app/classes/controllers/StudentOverviewController.php
+++ app/classes/controllers/StudentOverviewController.php
@@ -1,14 +1,22 @@
 <?php
 
-class StudentOverviewController extends \DS\Controller
+use DS\Controller;
+use DS\ErrorHandling;
+
+class StudentOverviewController extends Controller
 {
-    public function studentsOverview()
+    public function index()
     {
-        $students = $this->getStudents(); // Load the students
-        $this->view('students.overview', array('students' => $students)); // Show the view
+        $this->view('students.overview'); // Show the view
     }
 
-    private function getStudents() : object|null
+    public function ApiGetStudents(): string
+    {
+        $students = $this->getStudents();
+        return $this->formatRows($students);
+    }
+
+    private function getStudents(): object
     {
         /**
          * select * from students
@@ -20,7 +28,7 @@ class StudentOverviewController extends \DS\Controller
 
         try {
             // Statement to get all the students and there address and role
-            return $pdoOne
+            return (object)$pdoOne
                 ->select('students.StudentId, StudentName, StudentEmail, StudentMobile, Street, HouseNr, City, State, Country, r.RoleId,  RoleName, IF(r.UpdatedAt > students.UpdatedAt, r.UpdatedAt, students.UpdatedAt) as UpdatedAt')
                 ->from('students')
                 ->left('addresses a on students.AddressId = a.AddressId')
@@ -29,7 +37,30 @@ class StudentOverviewController extends \DS\Controller
         } catch (Exception $exception) {
             if (APP_MODE == "DEV") {
                 dd($exception); // If running in dev mode display the error else just show error 500
-            } else (new \DS\ErrorHandling(500))->ShowErrorScreen();
+            } else (new ErrorHandling(500))->ShowErrorScreen();
         }
+    }
+
+    private function formatRows(object $students): string
+    {
+        $html = "";
+
+        foreach ($students as $student) {
+            $html .= "
+        <tr>
+          <td>$student->StudentId</td>
+          <td>$student->StudentName</td>
+          <td>$student->StudentEmail</td>
+          <td>$student->StudentMobile</td>
+          <td>$student->Street $student->HouseNr</td>
+          <td>$student->City</td>
+          <td>$student->State</td>
+          <td>$student->Country</td>
+          <td>$student->RoleName</td>
+          <td>$student->UpdatedAt</td>
+         </tr>";
+        }
+
+        return $html;
     }
 }
