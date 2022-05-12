ad0cc06e4fce046666e8a4c84aa55dad058868a1
DarrenSanders <332030@student.mboutrecht.nl>
2022-05-12 20:34:18 +0200

    Updated overview.blade.php   - Added Head tag with title, and stylesheet import   - Removed foreach function replaced it for an api call   - Added fetch call for the data   - Added script to display a spinner/loader when data is being fetched

--- app/views/students/overview.blade.php
+++ app/views/students/overview.blade.php
@@ -1,39 +1,37 @@
-<body>
-<table>
-    <thead>
-    <tr>
-        <th>#</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Mobile</th>
-        <th>Address</th>
-        <th>Zip and City</th>
-        <th>State</th>
-        <th>Country</th>
-        <th>Role</th>
-        <th>Update</th>
-        <th>Delete</th>
-    </tr>
-    </thead>
-    <tbody>
-    @foreach($students as $student)
-        <tr>
-            <td>{{ $student->StudentId }}</td>
-            <td>{{ $student->StudentName }}</td>
-            <td>{{ $student->StudentEmail }}</td>
-            <td>{{ $student->StudentMobile }}</td>
-            <td>{{ $student->Street }} {{$student->HouseNr}}</td>
-            <td>null {{ $student->City }}</td>
-            <td>{{ $student->State }}</td>
-            <td>{{ $student->Country }}</td>
-            <td>{{ $student->RoleName }}</td>
-            <td>{{ $student->UpdatedAt }}</td>
+<head>
+    <title>Students</title>
+    <link rel="stylesheet" href="/public/assets/css/loading.css">
+</head>
 
+
+<body>
+<div class="container">
+    <table class="table">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Mobile</th>
+            <th>Address</th>
+            <th>Zip and City</th>
+            <th>State</th>
+            <th>Country</th>
+            <th>Role</th>
+            <th>Update</th>
+            <th>Delete</th>
         </tr>
-    @endforeach
-    </tbody>
-</table>
+        </thead>
+        <tbody class="student-table-body position-relative"></tbody>
+    </table>
+</div>
+
 </body>
 
+<script src="/public/assets/js/loader.js"></script>
+<script>
+    TableLoader('student-table-body'); // Load the loader animation
+    $(`.student-table-body`).load('/api/v1/students'); // Load the data
+</script>
 
 
